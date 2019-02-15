493c8af6d637e9b76b11f1795705b0923cbb7034
Szabo Vilmos <vili8899s@gmail.com>
2019-02-15 21:43:35 +0100

    challange7 added

--- main.js
+++ main.js
@@ -209,3 +209,49 @@ function fizzBuzz2() {
 }
 
 fizzBuzz2();
+
+// CHALLENGE 7: LONGEST WORD
+// Return the longest word of a string
+// If more than one longest word, put into an array
+// ex. longestWord('Hello, my name is Brad') === 'hello'
+// ex. longestWord('Hello there, my name is Brad') === ['hello', 'there']
+
+function longestWord(sen) {
+  // Create filtered array
+  const wordArr = sen.toLowerCase().match(/[a-z0-9]+/g);
+
+  // Sort by length
+  const sorted = wordArr.sort((a, b) => b.length - a.length);
+
+  // If multiple words, put into array
+  const longestWordArr = sorted.filter(
+    word => word.length === sorted[0].length
+  );
+
+  // Check if more than one array val
+  if (longestWordArr.length === 1) {
+    // Return the word
+    return longestWordArr[0];
+  } else {
+    return longestWordArr;
+  }
+}
+
+//function longestWord(sen) {
+
+//
+// let maxLetters = 0;
+// let outputArr = [];
+// sen.toLowerCase()
+//   .match(/\w+/g)
+//   .forEach(word => {
+//     if (!(word.length < maxLetters)) {
+//       if (word.length > maxLetters) {
+//         maxLetters = word.length;
+//         outputArr = [];
+//       }
+//       outputArr.push(word);
+//     }
+//   });
+// return outputArr.length > 1 ? outputArr : outputArr[0];
+//}
